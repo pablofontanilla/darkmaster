65610f78c4b32e498e3a0ccbff4a070fb2ca1174
pablofontanilla <chichaslocas@gmail.com>
2022-03-15 23:15:52 +0100

    Initial commit

---
+++ lambda/critical_tables.py
@@ -0,0 +1,118 @@
+critical_tables = {
+    "fire": {
+        5: "The air around the target warms up. +0 Hits.",
+        20: "Intense heat, the target sweats heavy, but little extra effect. +3 Hits.",
+        35:"Minor superficial burns. +8 hits and 1 Bleed.",
+        50:"Target's blinded for 1 round by hot smokes. +12 Hits, Stunned",
+        65:"Target's clothes catch fire. It takes 2 full-round actions to extinguish fire. Targets takes 8 Hits per round aflame until the fire's extinguished. After 10 rounds, all clothing's destroyed anyway.",
+        79:"Target is knocked down by a roaring flame blast. Stunned and +12 Hits. Target loses any organic foot and lower legs covering",
+        89:"Fire engulfs target's weapon arm. +10 Hits, 4 Bleed, Stunned. Drops any item and -30 activities for deep burns.",
+        99:"Fiery blast to the chest spins target around, disarms and Stuns him and inflicts +12 Hits. Target must Assess at the beginning of next round with a special -40 penalty.",
+        100:"Blast to the face. If the target's wearing some sort of head protection: Stunned, Blinded for 3 rounds, +15 Hits and 3 Bleed. If not: knocked unconscious, 6 Bleed, and a horrible scar (-10 BEA).",
+        106:"Fire engulf target's back. All items or clothing carried on the back is destroyed or useless. Target is knocked down and Stunned and takes +12 hits.",
+        109:"Burn to the upper leg. Leg useless due to deep burn and tissue loss. 15 Hits, 5 Bleed, and Stunned, -60 activities.",
+        110:"Blast to the neck. If the target is wearing protection: +12 Hits, Stunned and 4 Bleed. If not: target unconsious and dying in 12 rounds.",
+        116:"Shield arm blast. Any shield destroyed along with the hand that wields it. Stunned and 3 Bleed, -20 activity. If no shield: arm burned to the bone and useseless, +12 Hits, Stunned and 6 Bleed, -30 activity.",
+        119:"Target inhales scalding vapors. +12 Hits, 6 Bleed, target is Prone and incapable of taking any action while coughing for 6 rounds, then is Stunned.",
+        120:"Strike to midsection bathes target in flames. If the target's wearing protection, he's knocked down, Stunned, takes 8 Bleed and -60 activities for widespread burns. If not: legs destroyed, target dies in 6 unconscious rounds.",
+        126:"Powerful fiery blast. Target's knocked off his feet and sent flying through the air falling from 3 meters high. Target is disarmed, takes +15 Hits and is Stunned; furthermore the target takes a Moderate Impact Critical Strike from the fall.",
+        129:"Fiery blaze pulverizes target's weapon arm. Arm useless, +12 hits, 8 Bleed, Stunned and -40 activity.",
+        130:"Fire engulfs target's head. If the target is wearing metal armor: armor fuses on the head and the target dies in 5 rounds. If the target's wearing non-metal armor: armor destroyed, +12 Hits, 4 Bleed, Stunned and -20 activity. If the target's unarmored: knocked over and dies in 10 minutes due to extensive burns.",
+        136:"Target is bathed in flames. Extreme heat shock deals +50 Hits. Target falls into a 3-month coma.",
+        140:"Strike to the head. If the target's wearing helm, he's blinded for 2 weeks, +15 Hits. If not, dies in 6 rounds due to massive shock.",
+        146:"Blast to the chest. If the target's wearing chest armor: armor destroyed +12 Hits and 6 Bleed and target's Stunned. If not: +20 Hits, 12 Bleed, target is unconscious.",
+        149:"Target is totally engulfed in roaring flame. All organic material on target is destroyed, and metal melts. Target dies in 6 horrible, screaming rounds.",
+        150:"All that remains of the target is a smoking pile of charred teeth and bones."
+    },
+    "pierce": {
+        5:"Swoosh! That was close! No extra damage.",
+        20:"Graze to the shoulder. +3 Hits.",
+        35:"Thigh strike, +3 Hits. If the target's not wearing leg protection, the blow lunges deeper: 3 Bleed.",
+        50:"Minor scratch to the forearm: +2 Hits. If the target's unarmored on the forearm, he's also Stunned.",
+        65:"Strike grazes target's side: 1 Bleed and target's Stunned.",
+        79:"Direct shot to the chest. If the target's unarmored, the strike pierces deep: +8 Hits, 4 Bleed and Stunned. If the target's wearing armor: +4 Hits and 2 Bleed.",
+        89:"Strike to lower leg under the knee. Tendons and muscle torn. +3 Hits, Stunned and -25 activities.",
+        99:"Major abdominal wound. If the target's armored: +4 Hits, 3 Bleed. If not: +10 Hits, 6 Bleed, -20 activities and Stunned.",
+        100:"Strike to the ear. If the target's wearing a helm: +5 Hits, Stunned. If the target's unarmored: ear and skull pierced through; target is in a coma and takes 5 Bleed.",
+        106:"Thigh strike. If the target's wearing armor: +5 Hits, 2 Bleed. If not, very deep pierce: +7 Hits, 5 Bleed, Stunned and -30 activity.",
+        109:"Strike to lower leg. If the target's armored, torn muscle: +4 hits, -20 activities. If not, severed muscle: +6 Hits, -40 activity, Stunned.",
+        110:"Strike to the groin. If the target's wearing armor: +12 Hits, Stunned and -20 activities. If not: +18 Hits, 5 Bleed, -30 activities, Stunned; the character's permanently infertile.",
+        116:"Piercing strike to the chest. If the target's wearing rigid armor: +5 Hits, 4 Bleed and Stunned, -20 activities for a deep side cut. If not: lung pierced, +15 Hits, Stunned and -50 activity, dies in 6 hours.",
+        119:"Strike through the upper weapon arm sever tendons and muscle. +4 Hits, 4 Bleed, -20 activities, arm is useless.",
+        120:"Strike to the neck. If the target's wearing rigid neck protection: +5 Hits, 4 Bleed, Stunned. If not, artery punctured: +10 Hits, 15 Bleed, target Stunned.",
+        126:"Strike to the rear punctures the glute deep. +8 Hits, Stunned and -40 activity.",
+        129:"Strike through the armpit breaks shoulder's ligaments. +10 Hits, 6 Bleed, Stunned, -30 general activities and arm useless.",
+        130:"Strike to the forehead. If the target's wearing helm: +8 Hits, knocked unconscious for 30 minutes. If not: skull fractured, target unconsious and dies in 10 minutes.",
+        136:"Strike to the rear of the knee tears ligaments and damages muscle. Target's prone and at -60 activity, takes +8 Hits and 3 Bleed and is Stunned.",
+        140:"Strike through leg severs an artery. Target falls unconscious and Bleeds for 15 to death.",
+        146:"Strike through kidneys. +10 Hits. Target is knocked over and dies in 6 rounds of agony.",
+        149:"Direct shot to the heart. If the target's wearing rigid armor: +20 Hits, Stunned and 8 Bleed, -30 activity for deep cut. If not, target dies instantly.",
+        150: "Strike through the eye into the brain. Instant death."
+    },
+    "cut": {
+        5:"Weak strike doesn't cut through. No extra damage.",
+        20:"Minor calf wound. 1 Bleed.",
+        35:"Graze to shoulder. +2 Hits, 1 Bleed.",
+        50:"Minor chest wound. +3 Hits, 1 Bleed. -5 to activity.",
+        65:"Minor forearm cut. +4 Hits, 1 Bleed and target's Stunned.",
+        79:"Slice through side. If the target's wearing armor: +5 Hits, Stunned. If the target's unarmored: +10 Hits, 2 Bleed, Stunned and -10 activity.",
+        89:"Deeper cut to thigh. If the target's wearing leg armor: +3 Hits. If not, +6 Hits, 3 Bleed, Stunned and -20 to activity.",
+        99:"Slash through abdomen, +10 Hits and target's Stunned. If target's wearing armor, 3 Bleed; If target's unarmored, 6 Bleed and -30 activity.",
+        100:"Slash to the side of the head. If target is not wearing full metal helm, ear is cut off: +10 Hits, 3 Bleed and target is Stunned and -20 activity. If the target is wearing full helm, +4 Hits and target is Stunned.",
+        106:"Swing to the hand, roll 50/50 right/left. If the target is hit to the shield: +4 Hits, Stunned, shield unusable. If not: 1d5 fingers severed; 1 Bleed and -10 activity for each finger severed, Stunned.",
+        109:"Deep cut to the inside of the leg. Severs tendon and muscles: +12 Hits. Target is Stunned and 6 Bleed and -40 activity.",
+        110:"Deep cut to the lower abdomen. If the target's wearing armor: +10 Hits, Stunned and 6 Bleed, -30 activity. If the target is unarmored, he's gutted: dies in 12 inactive rounds.",
+        116:"Powerful strike to the chest. Target is Prone and Stunned and takes +12 hits and -30 activity. If the target's not wearing metal armor: 10 Bleed.",
+        119:"Powerful strike to the weapon's forearm. Target's disarmed, Stunned and takes +10 Hits. If target's not wearing arm amor: arm severed, 10 bleed and -40 activity.",
+        120:"Strike to the neck. If the target is wearing neck protection, it is destroyed and the target takes +10 Hits, is Stunned and 6 Bleed. If not: throat cut open, target dies in 8 rounds while Stunned.",
+        126:"Swing to the shoulder. Deep cut. If the target has rigid armor: +10 Hits, Stunned, 4 Bleed and -30 activity. If not, arm almost severed: +15 Hits, target's Stunned and 10 Bleed; arm useless and -50 activity.",
+        129:"Strike severs target's foot. Target is downed Prone and Stunned and Bleeds for 8. -50 activity.",
+        130:"Strike to the face. If the target's wearing helm: helm, nose and teeth destroyed, +12 Hits, Stunned, 4 Bleed and -40 activity and permanent scar (-20 BEA). If the target has no helm: face split in half, instand death.",
+        136:"Powerful strike severs weapon arm from the elbow, +12 Hits. Target is Stunned and takes 12 Bleed. Arm useless and -40 all other activities.",
+        140:"Strike severs lower leg from above the knee: +15 Hits. Target is Prone, Stunned and takes 15 Bleed. Can not walk and -60 all other activities.",
+        146:"Powerful slash accross the chest. If the target is wearing metal armor: +12 Hits, Stunned, 8 Bleed and -40 activity. If not: lungs destroyed & immediate heart failure - drops and dies in 6 rounds.",
+        149:"Strike through the side severs target's spine. Drops immediately and is paralyzed from the chest down and at -80 all other activities.",
+        150:"Target is beheaded. Instant death."
+    },
+    "impact": {
+        5 : "Flat but soft strike, no extra damage.",
+        20 : "Graze to shoulder. +5 Hits.",
+        35 : "Blow to the upper leg, painful bruise. Stunned and -10 activities.",
+        50  : "Beat to forearm. Drops weapon or shield. Target's stunned.",
+        65 : "Blow to side: broken breath. +4 Hits, target Stunned and at -40 for 1 round.",
+        79 : "Side strike. Target is Stunned and +6 Hits. If no rigid armor: 1d5 broken ribs, -5 penalty for each broken rib.",
+        89 : "Blow smashes weapon hand. Hand broken, +8 Hits, target Stunned and drops weapon, -20 activities.",
+        99 : "Abdomen strike. Target is doubled over in pain: Stunned and +12 hits. If no rigid armor: bruised muscles, +15 Hits and -40 activities.",
+        100 : "Flat strike to the head. If the target's wearing a helm: +10 Hits, Stunned and eardrum broken, -20 activities and -40 hearing Perception. If not: all of the above plus fractured skull, +30 hits and target is in a coma for 4 weeks.",
+        106 : "Blow to thigh. +12 Hits, Stunned and -20 activities. If no leg armor: cracked thighbone, +15 hits and -40 activities instead.",
+        109 : "Blow to the knee. Target is knocked down, +8 hits and -30 activity for a dislocation. If no leg armor: crushed knee, +20 Hits and -60 activities.",
+        110 : "Vicious strike to lower back. Target's knocked down and Stunned and takes +10 hits and -20 activity. If the target is not wearing waist protection: cracked vertebra, -50 activity and risk of being paralyzed from the waist down if not treated.",
+        116 : "Mighty blow to chest. Target's knocked down and drops any weapons, takes +10 Hits and is Stunned. If no rigid chest armor: broken sternum, -40 activity and +20 hits.",
+        119 : "Blow to forearm (50R/50L), fractured bones. If the shield is striken: shield useless and target's Stunned. If not: target drops whatever he's holding, takes +10 hits and is Stunned and at -30 activity.",
+        120 : "Neck strike. If the target's wearing rigid protection in the neck area: +12 Hits, Stunned, -20 activity and unable to speak or eat solid food until healed. If not, crushed windpipe: target's Stunned and dies in 12 rounds.",
+        126 : "Blow breaks shinbone. Target is down, Stunned and at -40 activity; +15 Hits.",
+        129 : "Blow to shoulder and upper arm. Arm broken and useless: +15 Hits, target's Stunned and at -40 activity.",
+        130 : "Powerful blow to the jaw. If the target's wearing full helm: jaw shattered, +20 Hits, Stunned and -30 activities, can not speak or eat until repaired. If not, jaw driven into brain, instant death.",
+        136 : "Blow shatters elbow in weapon arm. Arm useless and -40 other activities. Target's Stunned and takes +20 Hits.",
+        140 : "Blow to hip crushes bones. +35 Hits, Stunned and -75 activity. Target's knocked down.",
+        146 : "Powerful blow to lower abdomen crushes a variety of innards. If the target's wearing rigid armor: +12 Hits, Stunned, -20 activity. If not: internal bleeding, +20 Hits, Stunned and 12 Bleed, -40 activity.",
+        149 : "Blow to the side crushed ribcage and destroyes a variety of internal organs. Target drops unconscious and dies in 6 rounds.",
+        150 : "Inexorable blow to the head shatters skull and destroys brain. Instant death."
+    }
+}
+
+severity_table = {
+    "superficial": 0,
+    "light": 10,
+    "moderate": 20,
+    "grievous": 30,
+    "lethal": 50
+}
+
+def get_critical(table, roll):
+    current_table = critical_tables[table]
+    for number, text in current_table.items():
+        if roll > number:
+            continue
+        else:
+            return text
